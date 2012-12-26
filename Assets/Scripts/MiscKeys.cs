4f949c5948daf9570c7d1c9b98d2d348a99765cc
Stephen Ascher <sixsecondfiesta@gmail.com>
2012-12-26 04:43:10 -0500

    muzak, scene changes

---
+++ Assets/Scripts/MiscKeys.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class MiscKeys : MonoBehaviour
+{
+	AntialiasingAsPostEffect aa;
+
+	void Start()
+	{
+		aa = GetComponent<AntialiasingAsPostEffect>();
+		if(!aa)
+			Destroy(this);
+	}
+	
+	void Update()
+	{
+		if(Input.GetKeyDown("`"))
+			aa.enabled = !aa.enabled;
+		else if(Input.GetKeyDown("m"))
+		{
+			if(audio.isPlaying)
+				audio.Pause();
+			else
+				audio.Play();
+
+		}
+	}
+}
