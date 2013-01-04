9f1c3ffad1be50020dde7ebb8cbcd7c6fc4d870b
Stephen Ascher <sixsecondfiesta@gmail.com>
2013-01-03 23:29:17 -0500

    Animate angles, based on camera x-pos delta and input

--- Assets/Scripts/AnimateAngles.cs
+++ Assets/Scripts/AnimateAngles.cs
@@ -9,7 +9,10 @@ public class AnimateAngles : MonoBehaviour
 {
 	public float animationSpeed = 0.2f;
 
-	int angleCount = 5;
+	public Transform camTransform;
+	public float angleThreshold = 0.2f;
+
+	// int angleCount = 5;
 	int frameWidth = 32;
 
 	int walkFrames = 6;
@@ -42,8 +45,21 @@ public class AnimateAngles : MonoBehaviour
 			currentFrame = (currentFrame + 1) % walkFrames;
 			currentDelay = animationSpeed;
 		}
-		
-		Vector2 offset = new Vector2(2 * textureScale.x, 1f - (currentFrame + 1) * textureScale.y);
+
+		float dX = camTransform.position.x - transform.position.x + Input.GetAxis("Horizontal");
+
+		int xOffset = 2;
+
+		if(dX > 3 * angleThreshold)
+			xOffset = 4;
+		else if(dX > angleThreshold)
+			xOffset = 3;
+		else if(dX < -3 * angleThreshold)
+			xOffset = 0;
+		else if(dX < -angleThreshold)
+			xOffset = 1;
+			
+		Vector2 offset = new Vector2(xOffset * textureScale.x, 1f - (currentFrame + 1) * textureScale.y);
 
 		renderer.material.SetTextureOffset("_MainTex", offset);
 	}
