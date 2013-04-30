1642f0d133300a592c0d123584f17cff5ac3be71
Stephen Ascher <sixsecondfiesta@gmail.com>
2013-04-30 14:24:31 -0400

    ending stuff

--- Assets/Scripts/MoveAce.cs
+++ Assets/Scripts/MoveAce.cs
@@ -10,13 +10,35 @@ public class MoveAce : MonoBehaviour
 	public float moveSpeed = 2f;
 	public float maxX = 1.7f;
 
+	public Transform endSkull;
+
 	public float SteeringForce { get; private set; }
 
 	void Update()
 	{
+		Vector3 pos = transform.position;
+
+		if(pos.z > endSkull.position.z - 5f)
+		{
+			EndSteering();
+			return;
+		}
+
 		float dx = Input.GetAxis("Horizontal");
 
+		pos.x = Mathf.Clamp(pos.x + dx * Time.deltaTime * moveSpeed, maxX * -1, maxX);
+		transform.position = pos;
+
+		SteeringForce = dx;
+	}
+
+	void EndSteering()
+	{
 		Vector3 pos = transform.position;
+		Vector3 targetpos = endSkull.position + Vector3.right * 0.1f;
+
+		float dx = (targetpos.x - pos.x).Clamp(-1f, 1f);
+
 		pos.x = Mathf.Clamp(pos.x + dx * Time.deltaTime * moveSpeed, maxX * -1, maxX);
 		transform.position = pos;
 
